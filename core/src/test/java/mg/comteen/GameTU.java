a523232b199e7e7646364d91c375eac54e82e0ac
ramampiandra <ramampiandra.cpp@gmail.com>
2017-02-15 21:22:18 +0400

    add TU core

---
+++ core/src/test/java/mg/comteen/GameTU.java
@@ -0,0 +1,155 @@
+package mg.comteen;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import mg.comteen.common.Direction;
+import mg.comteen.common.Parameter;
+import mg.comteen.common.Result;
+
+public class GameTU {
+
+	private GameImpl game = null;
+
+	private String states;
+
+	@Before
+	public void setUp() throws Exception {
+		game = new GameImpl();
+		states = "BBBBBBBBB" + "BBBBBBBBB" + "BWBWEBWBW" + "WWWWWWWWW" + "WWWWWWWWW";
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/*
+	 * Simulate the game
+	 * Game position : 
+	 * 01 02 03 04 05 06 07 08 09
+	 * 10 11 12 13 14 15 16 17 18
+	 * 19 20 21 22 23 24 25 26 27
+	 * 28 29 30 31 32 33 34 35 36
+	 * 37 38 39 40 41 42 43 44 45
+	 * 
+	 */
+	@Test
+	public void handleGameTU() throws Exception {
+
+		// Test param one
+		/*
+		 * Parameter param = new Parameter();
+		 * 
+		 * param.setDirection(Direction.TOP_RIGHT);
+		 * param.setSourceStatePosition(31); Result<String> res =
+		 * game.handleGame(states, param);
+		 * 
+		 * setUp();
+		 */
+
+		// Test param two
+		Parameter param = new Parameter();
+		param.setDirection(-1);
+		param.setSourceStatePosition(31);
+		param.setDestStatePosition(23);
+		// withdraw move
+		param.setTypeMove(1);
+		// 1 Means Advance
+		Result<String> res = game.handleGame(states, param);
+		
+		param.setDirection(-1);
+		param.setSourceStatePosition(24);
+		param.setDestStatePosition(15);
+		// withdraw move
+		param.setTypeMove(-1);
+		res = game.handleGame(res.getData(), param);
+		
+		param.setDirection(-1);
+		param.setSourceStatePosition(15);
+		param.setDestStatePosition(7);
+		// withdraw move
+		param.setTypeMove(-1);
+		res = game.handleGame(res.getData(), param);
+		
+		param.setDirection(-1);
+		param.setSourceStatePosition(30);
+		param.setDestStatePosition(39);
+		// withdraw move
+		param.setTypeMove(-1);
+		res = game.handleGame(res.getData(), param);
+		
+		param.setDirection(-1);
+		param.setSourceStatePosition(14);
+		param.setDestStatePosition(24);
+		// withdraw move
+		param.setTypeMove(1);
+		res = game.handleGame(res.getData(), param);
+		
+		param.setDirection(-1);
+		param.setSourceStatePosition(24);
+		param.setDestStatePosition(23);
+		// withdraw move
+		param.setTypeMove(1);
+		res = game.handleGame(res.getData(), param);
+		
+		param.setDirection(-1);
+		param.setSourceStatePosition(23);
+		param.setDestStatePosition(14);
+		// withdraw move
+		param.setTypeMove(-1);
+		res = game.handleGame(res.getData(), param);
+		System.out.println(res.getMessage());
+		
+		param.setDirection(-1);
+		param.setSourceStatePosition(14);
+		param.setDestStatePosition(23);
+		// withdraw move
+		param.setTypeMove(-1);
+		res = game.handleGame(res.getData(), param);
+		System.out.println(res.getMessage());
+
+		printBoard(res);
+
+	}
+
+	@Test
+	public void setBoardTU() {
+		game.setBoard(states);
+		assertEquals(1, game.getBoard()[0][0]);
+		assertEquals(2, game.getBoard()[4][8]);
+	}
+
+	@Test
+	public void getStringBoardTU() {
+		game.setBoard(states);
+		String states = game.getStringBoard();
+		assertEquals("BBBBBBBBBBBBBBBBBBBWBWEBWBWWWWWWWWWWWWWWWWWWW", states);
+	}
+
+	@Test
+	public void setCurrentPositionTU() {
+		game.transformIndexTo2DPosition(1, 1);
+		int x = game.getCurrentPosition().getX();
+		int y = game.getCurrentPosition().getY();
+		assertEquals(0, x);
+		assertEquals(0, y);
+
+		game.transformIndexTo2DPosition(2, 1);
+		x = game.getCurrentPosition().getX();
+		y = game.getCurrentPosition().getY();
+		assertEquals(0, x);
+		assertEquals(1, y);
+	}
+
+	private void printBoard(Result<String> res) {
+		System.out.println(res.getData().substring(0, 9));
+		System.out.println(res.getData().substring(9, 18));
+		System.out.println(res.getData().substring(18, 27));
+		System.out.println(res.getData().substring(27, 36));
+		System.out.println(res.getData().substring(36, 45));
+	}
+
+}
---
+++ core/src/test/java/mg/comteen/RulesTU.java
@@ -0,0 +1,78 @@
+package mg.comteen;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import mg.comteen.common.Direction;
+import mg.comteen.common.Parameter;
+import mg.comteen.common.Player;
+import mg.comteen.common.Position;
+import mg.comteen.rule.RulesImpl;
+
+public class RulesTU {
+	
+	private RulesImpl rules = null;
+	
+	private int[][] board = {
+			{0,1,1,1,2,1,1,1,1},
+			{1,1,1,1,1,1,1,1,1},
+			{1,2,1,1,0,1,2,1,2},
+			{2,2,2,2,2,2,2,2,2},
+			{2,2,2,2,2,2,2,2,2}
+	};
+
+	@Before
+	public void setUp() throws Exception {
+		rules = RulesImpl.getInstance();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void checkIfNextPositionValidTU() {
+		
+		Parameter param = new Parameter();
+		Player player = new Player(1);
+		player.setLastPosition(new Position(0,3));
+		param.setCurrentPlayer(player);
+		param.setNextPosition(new Position(0, 3));
+		assertFalse(rules.checkIfNextPositionValid(board, param));
+		
+		param.setNextPosition(new Position(0, 2));
+		assertFalse(rules.checkIfNextPositionValid(board, param));
+		
+		param.setNextPosition(new Position(0, 0));
+		assertTrue(rules.checkIfNextPositionValid(board, param));
+		
+		param.setNextPosition(new Position(-1, 2));
+		assertFalse(rules.checkIfNextPositionValid(board, param));
+		
+		param.setNextPosition(new Position(0, 9));
+		assertFalse(rules.checkIfNextPositionValid(board, param));
+		
+		param.setNextPosition(new Position(-1, 9));
+		assertFalse(rules.checkIfNextPositionValid(board, param));
+	}
+	
+	@Test
+	public void eliminateAdversaryTU() {
+		//After move
+		Position nextPosition = new Position(3,1);
+		
+		Parameter param = new Parameter();
+		param.setDirection(Direction.TOP_RIGHT);
+		param.setNextPosition(nextPosition);
+
+		rules.eliminateAdversary(board, param);
+		assertEquals(0, board[2][2]);
+		assertEquals(0, board[1][3]);
+		assertEquals(2, board[0][4]);
+	}
+
+}
