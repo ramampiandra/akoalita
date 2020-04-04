21f476f3656d2c267f820750852da5e2cf2beeca
ramamj <ramamj@example.com>
2020-04-04 19:04:22 +0200

    add board

--- android/core/src/com/comteen/AkoalitaGame.java
+++ android/core/src/com/comteen/AkoalitaGame.java
@@ -13,15 +13,16 @@ public class AkoalitaGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("board.png");
 	}
 
 	@Override
 	public void render () {
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
