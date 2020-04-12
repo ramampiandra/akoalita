259fa4f9ccedd956bbba04ee02a6c1411b1a1c46
ramamj <ramamj@example.com>
2020-04-12 20:23:36 +0200

    Add Game/Screen/Stage to ligdx

--- android/core/src/com/comteen/AkoalitaGame.java
+++ android/core/src/com/comteen/AkoalitaGame.java
@@ -2,18 +2,31 @@ package com.comteen;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
 
 public class AkoalitaGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture boardImage;
+	private Sprite spriteOne, spriteTwo;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("board.png");
+		boardImage = new Texture("board.png");
+		spriteOne = new Sprite(new Texture("dark.png"));
+		spriteTwo = new Sprite(new Texture("light.png"));
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
@@ -21,14 +34,45 @@ public class AkoalitaGame extends ApplicationAdapter {
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
-		batch.draw(img,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(boardImage,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		batch.draw(spriteOne,140,560, 80,80);
+		batch.draw(spriteTwo,200,100,80,80);
+
 		batch.end();
+
+
+		final Vector3 testPoint = new Vector3();
+		Gdx.input.setInputProcessor(new InputAdapter() {
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				testPoint.set(screenX, screenY, 0);
+				camera.unproject(testPoint);
+				if(spriteOne.getBoundingRectangle().contains(screenX, screenY))
+					System.out.println("Touch Down");
+				return true;
+			}
+
+			public boolean touchUp (int screenX, int screenY, int pointer, int button) {
+				if(spriteOne.getBoundingRectangle().contains(screenX, screenY))
+					System.out.println("Touch Up");
+				return true;
+			}
+		});
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		boardImage.dispose();
 	}
 }
---
+++ android/core/src/com/comteen/controller/AkoalitaGameController.java
@@ -0,0 +1,11 @@
+package com.comteen.controller;
+
+import com.badlogic.gdx.InputAdapter;
+
+public class AkoalitaGameController extends InputAdapter {
+
+    public boolean touchDown (int screenX, int screenY, int pointer, int button) {
+        return true;
+    }
+
+}
